c6c837fcf0bad3789754dfd15190b5807477882e
김대휘 <kimdaewhi@naver.com>
2024-10-06 13:06:50 +0900

    main에서 사용하지 않는 import 제거

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api.member import auth_kis, auth_kakao, user
 from fastapi.middleware.cors import CORSMiddleware
 
-from common.utils.postgresql_helper import get_db, engine
+from common.utils.postgresql_helper import engine
 from contextlib import asynccontextmanager
 from app.models.base import Base
 
