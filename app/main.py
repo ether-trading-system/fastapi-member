994b3a6cc3379a89b6a0884b756eb3066565c75f
whgusqls007 <jo7hb@naver.com>
2024-11-19 23:11:19 +0900

    [FIX] 라우팅 URL 수정

--- app/main.py
+++ app/main.py
@@ -31,6 +31,6 @@ app.include_router(
     auth_kis.router, prefix="/public/api/member/auth-kis", tags=["auth_KIS"]
 )
 app.include_router(
-    auth_kakao.router, prefix="/public/api/member/auth-kis", tags=["auth_Kakao"]
+    auth_kakao.router, prefix="/public/api/member/auth-kakao", tags=["auth_Kakao"]
 )
-app.include_router(user.router, prefix="/public/api/member/auth-kis", tags=["user"])
+app.include_router(user.router, prefix="/public/api/member/user", tags=["user"])
